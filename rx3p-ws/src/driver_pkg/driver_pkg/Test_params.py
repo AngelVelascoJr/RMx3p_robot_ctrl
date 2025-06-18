8f1b25f28f6cedc96b8b023f65f64dcded8b7552
AngelVelascoJr <davidvp.advp@gmail.com>
2025-06-18 13:29:57 -0600

    fixed errors in params.py file

--- rx3p-ws/src/driver_pkg/driver_pkg/Test_params.py
+++ rx3p-ws/src/driver_pkg/driver_pkg/Test_params.py
@@ -7,11 +7,12 @@ from rcl_interfaces.msg import ParameterDescriptor, SetParametersResult
 class RobotParamSrv(Node):
     def __init__(self, node_name):
         super().__init__(node_name=node_name)
-        self.declare_parameter(name="sample_timer", value = 0.0, Descriptor = ParameterDescriptor(descrition="Cinifiguracion del timer de muestreo"))
+        self.declare_parameter(name="sample_timer", value = 0.0, descriptor = ParameterDescriptor(description="Cinifiguracion del timer de muestreo"))
         self.declare_parameters(namespace='',parameters = [('lin_vel', 0.1),('ang_vel', 0.04), ("int_label", Parameter.Type.STRING)])
         self.add_on_set_parameters_callback(self._on_set_params_callback)
+        self.get_logger().info("Param node working")
 
-    def on_set_params_callback(self, params:list[Parameter]):
+    def _on_set_params_callback(self, params:list[Parameter]):
         success_flag = True
         for param in params:
             if param.name == "sample_timer":
@@ -27,7 +28,7 @@ class RobotParamSrv(Node):
         return result_msg
 
 def init_node(): 
-    rclpy.init
+    rclpy.init()
     nodo = RobotParamSrv("x3p_params")
     rclpy.spin(nodo)
     rclpy.shutdown()
